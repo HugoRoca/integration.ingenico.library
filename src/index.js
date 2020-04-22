f93abe777c4a243401b09a8c5594d3822c1077ad
hugoroca <hugo.rock20@hotmail.com>
2020-04-22 15:37:46 -0500

    upload files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import forge from '../node_modules/node-forge/dist/forge.min.js'
+import connectsdk from './ingenico/connectsdk'
+
+console.log('entro');
+console.log(connectsdk);
+
+// apikey 7e009924db45fc40
+
+// secretapikey r06IODJrDvXhWodom/PO7+VSeT1ShP5GXfOi9/lWNUQ=
+
--- webpack.common.js
+++ webpack.common.js
@@ -2,10 +2,17 @@ const path = require('path');
 //const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './src/index.js',
+    /*main: './src/index.js',
+    libs: [
+      './node_modules/node-forge/dist/forge.min.js',
+      './src/ingenico/connectsdk.js',
+      './src/ingenico/connectsdk.noEncrypt.js'
+    ]*/
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
+    filename: '[name].js',
+    libraryTarget: 'var'
   },
   module: {
     rules: [
@@ -22,7 +29,11 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.js']
+    extensions: ['.js'],
+    alias: {
+      'forge': __dirname + '/node_modules/node-forge/dist/forge.min.js',
+      'ingenico': __dirname + '/src/ingenico/connectsdk.js'
+    }
   }
   //plugins: [new CleanWebpackPlugin()]
 }
